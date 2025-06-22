d8c1cf26f4d65afdc8f49807611316dc241167b5
vladv <vlad.vrn02@mail.ru>
2025-06-22 17:20:22 +0300

    add handlers of auth

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -1,5 +1,39 @@
 package auth
 
+import (
+	"context"
+
+	ssov1 "github.com/WALKWAY36/protos/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
 type serverAPI struct {
-	ssov1
+	ssov1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	req *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	return &ssov1.LoginResponse{
+		Token: req.GetPassword(),
+	}, nil
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	req *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverAPI) IsAdmin(
+	ctx context.Context,
+	req *ssov1.IsAdminRequest,
+) (*ssov1.IsAdminResponse, error) {
+	panic("implement me")
 }
