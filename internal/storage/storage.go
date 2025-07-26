56f8ad0c8d9b9f2ce1953b164874d9bcd2ce1085
vladv <vlad.vrn02@mail.ru>
2025-07-26 22:39:00 +0300

    fix naming

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,7 +3,7 @@ package storage
 import "errors"
 
 var (
-	ErrUserNotExists = errors.New("user already exists")
-	ErrUserNotFound  = errors.New("user not found")
-	ErrAppNotFound   = errors.New("app not found")
+	ErrUserExists   = errors.New("user already exists")
+	ErrUserNotFound = errors.New("user not found")
+	ErrAppNotFound  = errors.New("app not found")
 )
